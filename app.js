c31d0cf5567c48337804e10c955fdaa1480d48ad
JhonMedina <jhon-43769@outlook.com>
2023-12-15 16:20:28 -0400

    add changes

--- app.js
+++ app.js
@@ -22,17 +22,39 @@ app.post("/api/login", (req, res) => {
         res.json({
             token
         })
+        
     }); 
 });
 
 //treeh route
-app.post("/api/posts", (req, res) => {
-
-    res.json({  
-        mesaje:"Post fue creado"
+app.post("/api/posts", verifyToken , (req, res) => {
+    jwt.verify(req.token, 'secretKey' , (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        }else{
+            res.json({
+                mesaje: "Post se creo correctamente",
+                authData
+            });
+        }
     });
 });
 
+
+
+// Authorization: Bearer <token>
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers['authorization'];
+
+    if(typeof bearerHeader !== 'undefined'){
+     const bearerToken = bearerHeader.split(" ")[1];
+     req.token = bearerToken;
+     next();
+    }else{
+        res.sendStatus(403);
+    }
+}
+        
 
 
 
