60975d4b910272aa2975481034faa5a969382cad
JhonMedina <jhon-43769@outlook.com>
2023-12-15 15:31:54 -0400

    add routes

--- app.js
+++ app.js
@@ -3,12 +3,38 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 
-
+//First route
 app.get("/api", (req, res) => {
     res.json({
         mesaje:"express and jwt"
     })
 });
+
+// get user and token jwt
+app.post("/api/login", (req, res) => {
+    const user = {
+        id: 1,
+        name: "Jhon",
+        email: "jhon4369@gmail.com"
+    }
+
+    jwt.sign({ user }, 'secretkey', (err, token) => {
+        res.json({
+            token
+        })
+    }); 
+});
+
+//treeh route
+app.post("/api/posts", (req, res) => {
+
+    res.json({  
+        mesaje:"Post fue creado"
+    });
+});
+
+
+
 
 app.listen(3000, function(){
     console.log("Listen on port 3000");
